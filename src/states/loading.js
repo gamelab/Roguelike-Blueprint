866be28e281437793ce55a6b4cff924c85d819b8
Kieran <kieran@mahony.net.nz>
2015-02-19 21:12:02 +1300

    Revert "First Commit of this branch"
    This reverts commit 5212ef33060cf931c3a43fd0b79a2e3a4d0a7529.

--- src/states/loading.js
+++ src/states/loading.js
@@ -3,13 +3,13 @@
 */
 
 /**
-* Since we want to use the custom Kiwi.JS loader with the bobing kiwi/html5 logo and everything. We need to extend the KiwiLoadingScreen State.
+* Since we want to use the custom Kiwi.JS loader with the bobing kiwi/html5 logo and everything. We need to extend the KiwiLoadingScreen State.  
 * The KiwiLoadingScreen State is an extentsion of a normal State but it has some custom code to handle the loading/bobbing/fading of all the items, so if you override a method (like the preload) for example just make sure you call the super method.
-*
+* 
 * The parameters we are passing into this method are as ordered.
 * 1 - name {String} Name of this state.
 * 2 - stateToSwitch {String} Name of the state to switch to AFTER all the assets have loaded. Note: The state you want to switch to should already have been added to the game.
-* 3 - subfolder {String} The folder that the loading graphics are located at.
+* 3 - subfolder {String} The folder that the loading graphics are located at. 
 */
 var LoadingState = new KiwiLoadingScreen('LoadingState', 'IntroState', 'assets/img/loading/');
 /**
@@ -19,21 +19,17 @@ var LoadingState = new KiwiLoadingScreen('LoadingState', 'IntroState', 'assets/i
 */
 LoadingState.preload = function () {
 	 KiwiLoadingScreen.prototype.preload.call(this);
-
-    this.addAudio( 'loop', 'assets/sound/105197__cormi__monks-04.mp3' );
-    this.addAudio('swordStrike', 'assets/sound/77611__joelaudio__sfx-attack-sword-001.wav');
-    this.addAudio('boltStrike', 'assets/sound/65734__erdie__bow02.wav');
-
+	
     this.addSpriteSheet('toggleBtn', 'assets/img/buttons/toggleBtn.png', 100, 100);
 	this.addSpriteSheet('randomBtn', 'assets/img/buttons/randomBtn.png', 100, 100);
-	this.addSpriteSheet('tiles', 'assets/img/tiles_3.png', 32, 32);
-	this.addSpriteSheet('actors', 'assets/img/actors_4.png', 32, 32);
-
-	this.addSpriteSheet('heart', 'assets/img/heart_2.png', 32, 32);
-	this.addSpriteSheet('speed', 'assets/img/speed_3.png', 32, 32);
+	this.addSpriteSheet('tiles', 'assets/img/tiles.png', 32, 32);
+	this.addSpriteSheet('actors', 'assets/img/actors.png', 32, 32);
+	
+	this.addSpriteSheet('heart', 'assets/img/heart.png', 32, 32);
+	this.addSpriteSheet('speed', 'assets/img/speed.png', 32, 32);
 	this.addSpriteSheet('sword', 'assets/img/sword.png', 32, 32);
-	this.addSpriteSheet('loot', 'assets/img/loot_2.png', 32, 32);
-	this.addSpriteSheet('traps', 'assets/img/traps_2.png', 32, 32);
-	this.addSpriteSheet('shot', 'assets/img/shot_2.png', 32, 32);
+	this.addSpriteSheet('loot', 'assets/img/loot.png', 32, 32);
+	this.addSpriteSheet('traps', 'assets/img/traps.png', 32, 32);
+	this.addSpriteSheet('shot', 'assets/img/shot.png', 32, 32);
 
 };
